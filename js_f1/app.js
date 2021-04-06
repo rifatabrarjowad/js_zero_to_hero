bddd0db2ec118deaf610e58621b471f49c2085ce
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-06 14:42:17 +0600

    input

---
+++ js_f1/app.js
@@ -0,0 +1,14 @@
+var age = prompt("tell me your name :")
+
+console.log(age);
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+   
+  readline.question('Who are you?', name => {
+    console.log(`Hey there ${name}!`);
+    readline.close();
+  });
