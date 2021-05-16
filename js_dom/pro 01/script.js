037e20431862948312f1a499e48d023bdf356393
dev <rajdiprifat2004@gmail.com>
2021-05-16 21:25:13 +0600

    adding file

--- js_dom/pro 01/script.js
+++ js_dom/pro 01/script.js
@@ -3,3 +3,9 @@
 const x = function () {
     console.log(23);
 };
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+})
