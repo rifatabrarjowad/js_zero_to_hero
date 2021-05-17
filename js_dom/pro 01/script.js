8e93a83ea48c8c3cab65065660badcd29cc83e22
dev <rajdiprifat2004@gmail.com>
2021-05-17 22:05:28 +0600

    adding file

--- js_dom/pro 01/script.js
+++ js_dom/pro 01/script.js
@@ -8,8 +8,11 @@ document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
-})
 
-if (!guess) {
-    document.querySelector('.massage').textContent = 'No number !';
-}
+    if (!guess) {
+        document.querySelector('.massage').textContent = 'No number !';
+    } else if (guess === secretNumber) {
+        document.querySelector('.massage').textContent = 'Correct Number'
+    }
+
+})
